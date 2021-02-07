ef9098cd0409dcc1e4f4a4b53b1ca12d795d4fe0
Nikita <isip_n.a.strelcov@mpt.ru>
2021-02-07 15:00:20 +0300

    Name_commit

---
+++ Git/Not.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Git
+{
+    class Not
+    {
+        public static void lol()
+        {
+            Console.WriteLine("Lol");
+
+        }
+
+    }
+}
